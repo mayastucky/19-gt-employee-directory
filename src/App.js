2ffb88318204accf2118e89ffedfccba8379b770
Maya Stucky <mayaatstucky@gmail.com>
2020-08-01 11:10:45 -0400

    added api file and hardcoded employee component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React from "react";
 import "./App.css";
 import Title from "./components/Title";
+import EmployeeRow from "./components/EmployeeRow";
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Hello</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
       <Title />
+      <EmployeeRow />
     </div>
   );
 }
