328f9af175278fb440afb756afb22931e4c2adad
Maya Stucky <mayaatstucky@gmail.com>
2020-08-01 13:33:27 -0400

    employee table set up with 25 employees

--- src/components/EmployeeRow.jsx
+++ src/components/EmployeeRow.jsx
@@ -1,10 +1,38 @@
 import React from "react";
 // here we pass in the props (like attributes from JS)
 const EmployeeRow = (props) => {
+
+  const styles = {
+    tableStyle: {
+      text: "center",
+    },
+  };
+  
   return (
-    <div>
-      <h1>This will be the employee row here. </h1>
-      <p>{props.firstName}</p>
+    <div className="container">
+      <table class="table" style={styles.textStyle}>
+        <thead>
+          <tr>
+            <th scope="col">ID Picture</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Phone</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* we need a row with all the information of each employee */}
+          <tr>
+            <td>
+              <img src={props.src} alt={props.firstName}></img>
+            </td>
+            <td>
+              {props.firstName} {props.lastName}
+            </td>
+            <td>{props.email}</td>
+            <td>{props.phone}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
